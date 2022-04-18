74602b9e222a95d8a43285f778c68d3247eeaad7
sachin-gavali <98352918+sachin-gavali@users.noreply.github.com>
2022-04-18 14:47:41 +0530

    Add files via upload

---
+++ OOPs.java
@@ -0,0 +1,21 @@
+
+public class OOPs {
+    // class Collage{
+
+    public void FY(String student, int rollNo) {
+
+        // student = " SACHIN GAVALI ";
+        // rollNo = 18;
+        System.out.println(student);
+        System.out.println(rollNo);
+        System.out.println(student + rollNo);
+
+    }
+
+    public static void main(String[] args) {
+        OOPs sc = new OOPs();
+        sc.FY("SACHIN GAVALI", 19);
+
+    }
+
+}
